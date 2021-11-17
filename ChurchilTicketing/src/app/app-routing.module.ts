e80172a4c58996a0ce14f0e51506d5bb8e7f0aee
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-11-17 14:11:05 +0300

    Add more routes

--- ChurchilTicketing/src/app/app-routing.module.ts
+++ ChurchilTicketing/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './AuthGuard/auth.guard';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { AdminEditEventComponent } from './components/admin-edit-event/admin-edit-event.component';
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
+import { AdminViewEventComponent } from './components/admin-view-event/admin-view-event.component';
 import { EventDetailsComponent } from './components/event-details/event-details.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { PaymentDetailsComponent } from './components/payment-details/payment-details.component';
@@ -14,6 +16,10 @@ const routes: Routes = [
   { path: 'payment', component: PaymentDetailsComponent  },
   { path: 'login', component:  AdminLoginComponent  },
   { path: 'dashboard', component: AdminDashboardComponent, canActivate: [AuthGuard]  },
+  { path: 'admin-view/:id', component: AdminViewEventComponent, canActivate: [AuthGuard] },
+  { path: 'admin-edit/:id', component: AdminEditEventComponent, canActivate: [AuthGuard] },
+
+
 
 
 
